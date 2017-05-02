6b19dd36e7325756a5984da3933e658f95a17577
mko98 <maxkoning@casema.nl>
2017-05-03 00:30:28 +0200

    Update myrandomuser.js

--- MyRandomUser/myrandomuser.js
+++ MyRandomUser/myrandomuser.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var app = express();
 
+app.get('/json', function(request, response) {
+ response.json({
+ 'some_name': 'Value',
+ 'an_array_of_objects': [
+ {
+ 'another_name': 'Another value',
+ 'a_further_name': "A further value"
+ },
+ {'yet_another_name': 'Yet another value'}
+ ],
+ 'some_boolean': true,
+ 'some_integer': 42,
+ 'array_of_ints': [
+ 2, 3, 5, 7, 11, 13
+ ],
+ 'array_of_strings': [
+ "twee", "drie", "vijf", "zeven"
+ ]
+ })
+})
+
 
 
 app.get('/', function(request, response) {
